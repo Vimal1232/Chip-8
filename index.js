082fa000b666be339ea050944bc6847ef224b5ab
Vimal Chaudhary <86522515+Vimal1232@users.noreply.github.com>
2025-04-23 12:14:50 +0530

    Major Opcodes Implemented

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ class chip8 {
     this.i = 0;
     this.v = new Uint8Array(16);
     this.display = new Uint8Array(64 * 32);
+    this.keypad = new Uint8Array(16);
     this.fontset = [
       0xf0, 0x90, 0x90, 0x90, 0xf0, 0x20, 0x60, 0x20, 0x20, 0x70, 0xf0, 0x10,
       0xf0, 0x80, 0xf0, 0xf0, 0x10, 0xf0, 0x10, 0xf0, 0x90, 0x90, 0xf0, 0x10,
@@ -19,6 +20,25 @@ class chip8 {
     ];
 
     this.fontsetLoad();
+    this.mode = "cosmac";
+    this.keymap = {
+      1: 0x1,
+      2: 0x2,
+      3: 0x3,
+      4: 0xc,
+      q: 0x4,
+      w: 0x5,
+      e: 0x6,
+      r: 0xd,
+      a: 0x7,
+      s: 0x8,
+      d: 0x9,
+      f: 0xe,
+      z: 0xa,
+      x: 0x0,
+      c: 0xb,
+      v: 0xf,
+    };
   }
 
   fontsetLoad() {
@@ -53,6 +73,13 @@ class chip8 {
           this.display.fill(0);
           break;
         }
+
+        if (opcode === 0x0ee) {
+          this.pc = this.stack.pop();
+          break;
+        }
+        break;
+
       case 0x1:
         this.pc = nnn;
         break;
@@ -65,6 +92,192 @@ class chip8 {
       case 0xa:
         this.i = nnn;
         break;
+      case 0x2:
+        this.stack.push(this.pc);
+        this.pc = nnn;
+        break;
+
+      case 0x8:
+        if (n === 0) {
+          this.v[x] = this.v[y];
+          break;
+        }
+
+        if (n === 1) {
+          this.v[x] = this.v[x] | this.v[y];
+          break;
+        }
+
+        if (n === 2) {
+          this.v[x] = this.v[x] & this.v[y];
+          break;
+        }
+
+        if (n === 3) {
+          this.v[x] = this.v[x] ^ this.v[y];
+          break;
+        }
+
+        if (n === 4) {
+          this.v[x] += this.v[y];
+          if (this.v[x] > 255) {
+            this.v[0xf] = 1;
+            break;
+          } else {
+            this.v[0xf] = 0;
+            break;
+          }
+        }
+        if (n === 5) {
+          this.v[0xf] = this.v[x] > this.v[y] ? 1 : 0;
+          this.v[x] = this.v[x] - this.v[y];
+          break;
+        }
+
+        if (n === 7) {
+          this.v[0xf] = this.v[y] > this.v[x] ? 1 : 0;
+          this.v[x] = this.v[y] - this.v[x];
+          break;
+        }
+
+        if (n === 6) {
+          if (this.mode === "cosmac") {
+            this.v[0xf] = this.v[y] & 0x1;
+            this.v[x] = this.v[y] >> 0x1;
+            break;
+          } else {
+            this.v[0xf] = this.v[x] & 0x1;
+            this.v[x] = this.v[x] >> 0x1;
+            break;
+          }
+        }
+
+        if (n === 0xe) {
+          if (this.mode === "cosmac") {
+            this.v[0xf] = (this.v[y] & 0x80) >> 7;
+            this.v[x] = (this.v[y] << 0x1) & 0xff;
+            break;
+          } else {
+            this.v[0xf] = (this.v[x] & 0x80) >> 7;
+            this.v[x] = (this.v[x] << 0x1) & 0xff;
+            break;
+          }
+        }
+        break;
+      case 0xb:
+        this.pc = nnn + this.v[0];
+        break;
+      case 0xc:
+        this.v[x] = Math.floor(Math.random() * 256) & nn;
+        break;
+
+      case 0x3:
+        if (this.v[x] === nn) {
+          this.pc += 2;
+        }
+        break;
+
+      case 0x4:
+        if (this.v[x] !== nn) {
+          this.pc += 2;
+        }
+        break;
+      case 0x5:
+        if (this.v[x] === this.v[y]) {
+          this.pc += 2;
+        }
+        break;
+      case 0x9:
+        if (this.v[x] != this.v[y]) {
+          this.pc += 2;
+        }
+        break;
+
+      case 0xe:
+        if (n === 0xe) {
+          if (this.keypad[this.v[x]]) {
+            this.pc += 2;
+          }
+          break;
+        }
+
+        if (n === 0x1) {
+          if (!this.keypad[this.v[x]]) {
+            this.pc += 2;
+          }
+          break;
+        }
+
+      case 0xf:
+        if (nn === 0x07) {
+          this.v[x] = this.delayTimer;
+          break;
+        }
+
+        if (nn === 0x15) {
+          this.delayTimer = this.v[x];
+          break;
+        }
+
+        if (nn === 0x18) {
+          this.soundTimer = this.v[x];
+          break;
+        }
+
+        if (nn === 0x1e) {
+          this.i += this.v[x];
+          break;
+        }
+
+        if (nn === 0x0a) {
+          let key = false;
+
+          for (let i = 0; i < this.keypad.length; i++) {
+            if (this.keypad[i] === 1) {
+              this.v[x] = i;
+              key = true;
+              break;
+            }
+          }
+
+          if (!key) {
+            this.pc -= 2;
+            return;
+          } else {
+            this.pc += 2;
+            break;
+          }
+        }
+
+        if (nn === 0x29) {
+          this.i = 0x50 + this.v[x] * 5;
+          break;
+        }
+
+        if (nn === 0x33) {
+          const first = Math.floor(this.v[x] / 100);
+          const second = Math.floor((this.v[x] % 100) / 10);
+          const third = Math.floor(this.v[x] % 10);
+
+          this.memory[this.i] = first;
+          this.memory[this.i + 1] = second;
+          this.memory[this.i + 2] = third;
+          break;
+        }
+
+        if (nn === 0x55) {
+          for (let i = 0; i <= x; i++) {
+            this.memory[this.i + i] = this.v[i];
+          }
+          break;
+        }
+        if (nn === 0x65) {
+          for (let i = 0; i <= x; i++) {
+            this.v[i] = this.memory[this.i + i];
+          }
+          break;
+        }
+
       case 0xd:
         const X = this.v[x];
         const Y = this.v[y];
@@ -111,7 +324,17 @@ const ctx = canvas.getContext("2d");
 
 const Emu = new chip8();
 
-const file = fetch("/IBM2.ch8")
+window.addEventListener("keydown", (e) => {
+  const key = Emu.keymap[e.key];
+  if (key !== null) Emu.keypad[key] = 1;
+});
+
+window.addEventListener("keyup", (e) => {
+  const key = Emu.keymap[e.key];
+  if (key !== null) Emu.keypad[key] = 0;
+});
+
+const file = fetch("/octo.ch8")
   .then((response) => response.arrayBuffer())
   .then((data) => {
     const rom = new Uint8Array(data);
@@ -127,7 +350,7 @@ function draw() {
       const index = x + y * 64;
       const pixel = Emu.display[index];
       if (pixel) {
-        ctx.fillStyle = "white";
+        ctx.fillStyle = "Green";
         ctx.fillRect(x * 10, y * 10, 10, 10);
       } else {
         ctx.fillStyle = "black";
@@ -135,6 +358,5 @@ function draw() {
       }
     }
   }
-
   requestAnimationFrame(draw);
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,14 @@
 This is A Chip - 8 Emulator using Javascript
 
-Done Some tests Like the Famouse IBM Logo
+## Features
 
-The Emulator is Incomplete and not all opcodes are implemented
+- 32x64 pixel display
+- 4kb memory
+- 16 registers
+- Simple instruction set architecture
+
+## Controls
+
+- WASD for navigation
+- Z for selecting an option
+- X for canceling or going back
